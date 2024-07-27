1f51305fe34996e52b4682c3c758805bc9292530
Pine Mizune <pinemz@gmail.com>
2024-07-27 20:56:30 +0900

    fix: do not use async keyword

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,10 @@
 export declare class Keychain {
   constructor(service: string, account: string)
-  async getOrCreatePassword(): string
+  getOrCreatePassword(): Promise<string>
 }
 
 export declare class SafeStorage {
   constructor(password: string)
-  async encryptString(plainText: string): string
-  async decryptString(encrypted: string): string
+  encryptString(plainText: string): Promise<string>
+  decryptString(encrypted: string): Promise<string>
 }
