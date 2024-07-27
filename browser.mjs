dad56ada67f2ce2c6337ec42c8693ee6d18eb20e
Pine Mizune <pinemz@gmail.com>
2024-07-27 09:25:26 +0900

    chore: initial commit

---
+++ browser.mjs
@@ -0,0 +1,4 @@
+import { Keychain } from './lib/keychain_browser.mjs'
+import { SafeStorage } from './lib/safe_storage.mjs'
+
+export { Keychain, SafeStorage }
---
+++ node.mjs
@@ -0,0 +1,4 @@
+import { Keychain } from './lib/keychain_node.mjs'
+import { SafeStorage } from './lib/safe_storage.mjs'
+
+export { Keychain, SafeStorage }
