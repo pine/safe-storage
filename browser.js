4e9454efa2e24ba2540397321b5c0ca88fabf035
Pine Mizune <pinemz@gmail.com>
2024-08-01 09:20:40 +0900

    fix: fix file extension

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-import { Keychain } from './lib/keychain_browser.mjs'
+import { Keychain } from './lib/keychain_browser.js'
 import { SafeStorage } from './lib/safe_storage.js'
 
 export { Keychain, SafeStorage }
--- node.js
+++ node.js
@@ -1,4 +1,4 @@
-import { Keychain } from './lib/keychain_node.mjs'
+import { Keychain } from './lib/keychain_node.js'
 import { SafeStorage } from './lib/safe_storage.js'
 
 export { Keychain, SafeStorage }
